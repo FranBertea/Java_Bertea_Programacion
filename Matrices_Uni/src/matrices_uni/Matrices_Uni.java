39aaf96db2fdc17c57f212bbaa87fe4d4b5351ce
FranBertea <80895162+FranBertea@users.noreply.github.com>
2021-03-19 08:52:42 -0300

    Add files via upload

---
+++ Matrices_Uni/src/matrices_uni/Matrices_Uni.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrices_uni;
+
+import java.util.Scanner;
+/**
+ *
+ * @author pc06
+ */
+public class Matrices_Uni {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        
+        int num = 5;
+        int i;
+        int[] numeros=new int [num];
+        
+        System.out.println("Ingrese 5 numeros: ");
+        for (i=0;i<num;i++){
+          numeros [i]=teclado.nextInt();
+        }
+         System.out.println("Los 5 numeros son: "); 
+        for (i=0;i<num;i++){   
+         System.out.println(numeros[i]);
+       
+        }
+                
+    }
+}
+    
+
