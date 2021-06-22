4c8b298993db96e7bc312ccf713d2e4f155c50d5
FranBertea <80895162+FranBertea@users.noreply.github.com>
2021-06-22 17:18:12 -0300

    Add files via upload

---
+++ JavaGUI/src/Gui2/JavaGUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gui2;
+
+/**
+ *
+ * @author franb
+ */
+public class JavaGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         new Bertea_Gui2().setVisible(true);
+         
+    }
+    
+}
