8c8e0981c36f3af907ea5622b93c510974e0a80f
FranBertea <80895162+FranBertea@users.noreply.github.com>
2021-07-26 10:30:53 -0300

    Add files via upload

---
+++ Clase 26 de Julio/src/clase/pkg26/de/julio/Clase26DeJulio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase.pkg26.de.julio;
+
+/**
+ *
+ * @author pc06
+ */
+public class Clase26DeJulio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         new Clase_26_de_Julio ().setVisible(true);
+    }
+    
+}
